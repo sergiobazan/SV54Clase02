1597316474ece08036fa11d2ca2f6142d05edd98
Sergio Bazan <55011041+GiornoGiovana@users.noreply.github.com>
2023-04-03 22:23:03 -0500

    Create SideBar Component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div class="sidebar-container">
+        <h2>{{title}}</h2>
+        <p>{{description}}</p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Sidebar",
+        props: {
+            title: String,
+            description: String
+        }
+    }
+</script>
+
+<style>
+
+.sidebar-container {
+    border-radius: .5rem;
+    border: 1px solid black;
+}
+
+.sidebar-container h2 {
+    margin: 0;
+    border-bottom: 1px solid black;
+}
+
+</style>
