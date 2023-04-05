083ea6d114b32125a8dff19a2489d98874020ad3
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-04-04 21:14:24 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import footerComponent from './components/footer-component.vue';
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Test title" />
+  </div> -->
+  <!-- <HelloWorld msg="Test title" /> -->
   <footerComponent email="venta@library.com" number="981622558" direction="Los Manzanos 398"/>
 </template>
 
