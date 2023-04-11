a952d3462555e5fd39ecd368b29e9ea0ab892ae4
Emilia <u201914541@upc.edu.pe>
2023-04-10 22:09:55 -0500

    Added i18n to first section of form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,40 +1,98 @@
 <template>
     <form>
-        <!-- Inputs van aqui -->
-        
-        <!-- calendario -->
+
+      <label for ="code">{{ $t('Code')}}</label>
+        <InputMask id="basic" v-model="value" mask="99-999999" placeholder="99-999999" />
+        <Button label="Validate"/>
+
+      <label for="name">{{ $t('Name')}}</label>
+      <pv-inputText placeholder="Product name"/>
+
+      <label for="category">{{ $t('Category')}}</label>
+      <pv-dropdown v-model="selectedCat" :options="categorias" optionLabel="name" placeholder="Category" class="w-full md:w-14rem" />
+
         <label for="date">Date added:</label>
         <Calendar id="date" v-model="date" dateFormat="dd/mm/yy" />
         
-        <!--Numero cantidad-->
         <label for="inputnumber">Cuantity:</label>
         <InputNumber id="inputnumber" v-model="value2" inputId="withoutgrouping" :useGrouping="false" />
 
-        <!--Area de texto-->
         <label for="description">Description:</label>
         <Textarea v-model="value" rows="5" cols="30" />
 
+      <div class="digital-version">
+        <Checkbox v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
+        <label for="ingredient1" class="ml-2"> Digital Version </label>
+      </div>
 
-        
-    
+      <div class="radio-container">
+        <div class="flex align-items-center">
+          <RadioButton v-model="Status" inputId="Status1" name="status" value="Active" />
+          <label for="ingredient1" class="ml-2">Active</label>
+        </div>
+
+        <div class="flex align-items-center">
+          <RadioButton v-model="Status" inputId="Status2" name="status" value="Suspended" />
+          <label for="ingredient2" class="ml-2">Suspended</label>
+        </div>
+
+        <div class="flex align-items-center">
+          <RadioButton v-model="Status" inputId="Status3" name="status" value="Inactive" />
+          <label for="ingredient3" class="ml-2">Inactive</label>
+        </div>
+      </div>
+
+      <div class="btn-container">
+        <Button label="Save" />
+        <Button label="Cancel" />
+      </div>
+
+
+
     </form>
 </template>
 
 <script>
     export default {
-        name: 'Form'
+        name: 'Form',
+      data() {
+          return {
+            selectedCat: null,
+            categorias : [
+              { name: 'Food', code: '001' },
+              { name: 'Appliances', code: '002' },
+              { name: 'Electronics', code: '003' }
+            ]
+          }
+      }
     }
+
 </script>
 
+
 <style>
-form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-    border: 1px solid black;
-    border-radius: 1rem;
-    padding: 1rem;
-    height: 100%;
-}
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        border: 1px solid black;
+        border-radius: 1rem;
+        padding: 1rem;
+        height: 100%;
+    }
+    .digital-version {
+        width: 100%;
+    }
+    .radio-container {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+    .btn-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        gap: .75rem;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -13,11 +13,27 @@ import Calendar from 'primevue/calendar';
 import InputNumber from 'primevue/inputnumber';
 import Textarea from 'primevue/textarea';
 
+import InputText from "primevue/inputtext";
+import InputMask from "primevue/inputmask";
+import Dropdown from "primevue/dropdown";
+
+import Checkbox from 'primevue/checkbox';
+import RadioButton from 'primevue/radiobutton';
+
+import i18n from "./locales/i18n.js";
+
 const app = createApp(App);
 
 app.component("Button", Button);
+app.component('pv-inputText', InputText);
+app.component('inputMask', InputMask)
+app.component('pv-dropdown',Dropdown)
 app.component('Calendar',Calendar);
 app.component('InputNumber',InputNumber);
+app.component('Checkbox',Checkbox);
+app.component('RadioButton',RadioButton);
 app.component('Textarea',Textarea)
+
 app.use(PrimeVue);
+app.use(i18n);
 app.mount("#app");
