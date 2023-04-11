064b5acc6ff6bde84b39afc1782767184b6c9b12
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-04-10 21:51:34 -0500

    update

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,22 @@
 <template>
     <form>
         <!-- Inputs van aqui -->
+        
+        <!-- calendario -->
+        <label for="date">Date added:</label>
+        <Calendar id="date" v-model="date" dateFormat="dd/mm/yy" />
+        
+        <!--Numero cantidad-->
+        <label for="inputnumber">Cuantity:</label>
+        <InputNumber id="inputnumber" v-model="value2" inputId="withoutgrouping" :useGrouping="false" />
+
+        <!--Area de texto-->
+        <label for="description">Description:</label>
+        <Textarea v-model="value" rows="5" cols="30" />
+
+
+        
+    
     </form>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -9,9 +9,15 @@ import "primevue/resources/themes/saga-purple/theme.css"; // theme
 import "primevue/resources/primevue.min.css"; // core
 import "primeicons/primeicons.css"; // icons
 import Button from "primevue/button";
+import Calendar from 'primevue/calendar';
+import InputNumber from 'primevue/inputnumber';
+import Textarea from 'primevue/textarea';
 
 const app = createApp(App);
 
 app.component("Button", Button);
+app.component('Calendar',Calendar);
+app.component('InputNumber',InputNumber);
+app.component('Textarea',Textarea)
 app.use(PrimeVue);
 app.mount("#app");
